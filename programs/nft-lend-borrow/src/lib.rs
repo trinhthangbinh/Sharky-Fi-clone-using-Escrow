c5f3a13c727e7d9d52de4a40e9a90bbb670996e6
trinhthangbinh <164190238+trinhthangbinh@users.noreply.github.com>
2024-03-20 20:50:06 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("5vo9xbPekJJJqisGfMChqpVkbAACAXfQnQszYAeV33UF");
 
 #[program]
 pub mod nft_lend_borrow {
